1f57caa4708726527c96a0777162920c3a496b45
ana-ruth <114896716+ana-ruth@users.noreply.github.com>
2024-06-25 15:35:04 -0700

    Exercise 3 Complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(givenString) {
+    let reversed = "";
+    for(let i=givenString.length-1;i>=0;i--)
+        {
+            reversed+= givenString[i];
+        }
+    return reversed;
 };
 
 // Do not edit below this line
