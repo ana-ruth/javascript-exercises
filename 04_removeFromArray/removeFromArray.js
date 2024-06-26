0044c97a17b573304a72195a80587847f51c3644
ana-ruth <114896716+ana-ruth@users.noreply.github.com>
2024-06-26 12:26:33 -0700

    exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray,removeElement,secondE) {
+   
+    let array = [];
+    for (let i=0;i<givenArray.length;i++)
+    {
+        if(givenArray[i] == removeElement || givenArray[i] == secondE)
+            {
+                continue;
+            }
+        array.push(givenArray[i])
+    }
+    return array;
+    
+
 };
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
